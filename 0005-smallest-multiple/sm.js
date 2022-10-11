2ab4a5bfac59f0db6d0f9da1ac1dfb60ddf2311c
Dean Lovett <dean.lovett@gmail.com>
2022-10-10 20:21:43 -0700

    add comments

--- 0005-smallest-multiple/sm.js
+++ 0005-smallest-multiple/sm.js
@@ -15,46 +15,6 @@ Problem 5
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
 
-/*
-debugging boilerplate
-*/
-const dline = '========================';
-const sline = '------------------------';
-
-let isSilent = true;
-let isVerboseLog = false;
-let isDebugLog = false;
-
-function progdot(){
-    if(isSilent)
-        process.stdout.write('.');
-}
-function clog(...args){
-    if(isSilent)
-        console.log(...args);
-}
-function dlog(...args){
-    if(isDebugLog && isSilent)
-        console.log(...args);
-}
-function vlog(...args){
-    if(isVerboseLog && isSilent)
-        console.log(...args);
-}
-
-function clogdline(){
-    clog(dline);
-}
-function clogsline(){
-    clog(sline);
-}
-
-function dlogdline(){
-    dlog(dline);
-}
-function dlogsline(){
-    dlog(sline);
-}
 
 /*
 function to solve the problem
@@ -80,14 +40,14 @@ function getNextPrime(){
 }
 
 function factor(n){
-    dlog(`  inside factor(n:${n})`)
+    __.dlog(`  inside factor(n:${n})`)
 
     let w = n;
     let maxpidx = primes.length-1;    
     let stop = Math.trunc( Math.sqrt(n) ) + 1;
     // let lastPrime = primes[maxpidx];
 
-    dlog(`    right before while(stop>primes[maxpidx:${maxpidx}])`)
+    __.dlog(`    right before while(stop>primes[maxpidx:${maxpidx}])`)
 
     while(stop>primes[maxpidx]){
         lastPrime = getNextPrime();
@@ -95,10 +55,10 @@ function factor(n){
     }
     let fact = new Map();
 
-    dlog(`    right before or(let i=0; i<=maxpidx:${maxpidx} && primes[i:0]:${primes[0]} < stop:${stop}; ++i){`)
+    __.dlog(`    right before or(let i=0; i<=maxpidx:${maxpidx} && primes[i:0]:${primes[0]} < stop:${stop}; ++i){`)
 
     for(let i=0; w>1 && i<=maxpidx && primes[i]<=stop; ++i){
-        dlog(`      w: ${w}`)
+        __.dlog(`      w: ${w}`)
         let pt = primes[i];
         fact.set(pt,0);
         if( factMap.has(w) ){ // w is prime
@@ -116,15 +76,15 @@ function factor(n){
             if( w === 1) break;
         }
     }
-    dlog();
-    dlog(`factors for n:${n}`);
-    dlog(fact);
-    dlog();
+    __.dlog();
+    __.dlog(`factors for n:${n}`);
+    __.dlog(fact);
+    __.dlog();
     return fact;
 }
 
 function smallestmult(target){
-    dlog(`inside smallestmult(target:${target})`)
+    __.dlog(`inside smallestmult(target:${target})`)
 
     let ret = 1;
     
@@ -151,81 +111,26 @@ driver
 */
 let nums = [];
 
-function parseCommandLineArgs(){
-    
-    let isError = false;
-    let unknownArgs = [];
-    const myArgs = process.argv.slice(2);
-    let debugstrarr=['-d','--debug'];
-    let verbosearr=['-v','--verbose'];
-    let silentarr=['-s','--silent'];
-    let smyArgs = myArgs.slice();
-    for( ; myArgs.length >= 1; myArgs.shift()){
-        let test=parseInt(myArgs[0]);
-        if( debugstrarr.includes(myArgs[0].toLowerCase()) ){
-            isDebugLog = true;
-            isVerboseLog = true;
-            isSilent = true; // what the hell, turn it on here
-            // dlog('calling args: ',myArgs)
-            // dlog('debug set to TRUE')
-        }
-        else if( verbosearr.includes(myArgs[0].toLowerCase()) ){
-            isDebugLog = true;  // what the hell, turn it on here
-            isVerboseLog = true;
-            isSilent = true;
-            // clog('calling args: ',myArgs)
-            // clog('verbose set to TRUE')
-            // clog('  debug set to TRUE')
-        }
-        else if( silentarr.includes(myArgs[0].toLowerCase()) ){
-            isDebugLog = false;  // what the hell, turn it on here
-            isVerboseLog = false;
-            isSilent = false;
-        }
-        else if(test>0){
-            nums.push(test);
-        }
-        else{
-          isError = true;
-          unknownArgs.push(myArgs[0]);
-        }
-        if(isError){
-            console.log();
-            console.log('unknown args:',unknownArgs )
-            // let shelp = myArgs[0].toLowerCase();
-            // if( shelp === '-h' || '--help')
-            console.log();
-            console.log('usage: node sm.js [number ...][-d] [--debug] [-v] [--verbose]');
-            console.log('       -d or --debug. : extra debugging output');
-            console.log('       -v or --verbose: extra chatty output');
-            console.log();
-            console.log('       Prints out the smallest positive number that is evenly divisible by all of the numbers from 1 to 20.');
-            console.log();
-            return(false);
-        }
-    }
-    dlog()
-    dlog('calling args: ',myArgs)
-    if(isVerboseLog) clog('verbose set to TRUE')
-    if(isDebugLog)   clog('  debug set to TRUE')
-    return true;
-}
-
-if( ! parseCommandLineArgs() ) return(0);
+if( ! __.parseCommandLineArgs('sm') ) return(0);
+nums = __.getNumbersFromCommandLine()
 
 let primes = [2,3,5,7,11,13,17,19];
 let factMap = new Map();
 primes.forEach((e,i,arr)=>{
     factMap.set(e,0);
 });
-dlog(`factMap`);
-dlog(factMap);
-dlog();
+__.dlog(`primes:`);
+__.dlog(primes);
+__.dlog();
+__.dlog(`factMap`);
+__.dlog(factMap);
+__.dlog();
 
 if( nums.length == 0) nums.push(10);
 while(nums.length>0){
     let tar = nums.shift()
     let val = smallestmult( tar );
-    console.log()
-    console.log(`target: ${tar}  value: ${val}`);
+    __.clog()
+    __.clog(`target: ${tar}  value: ${val}`);
+    __.slog(tar, val);
 }
