62f605ba759ab3b3e1072cafce69ebc53300a6d9
Dean Lovett <dean.lovett@gmail.com>
2022-10-10 20:03:37 -0700

    add imporved algo

--- 0004-largest-palindrome-product/lpp.js
+++ 0004-largest-palindrome-product/lpp.js
@@ -16,41 +16,6 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
 /*
-debugging boilerplate
-*/
-const dline = '========================';
-const sline = '------------------------';
-
-let isConLog = true;
-let isDebugLog = false;
-
-function progdot(){
-    if(isConLog)
-        process.stdout.write('.');
-}
-function clog(...args){
-    if(isConLog)
-        console.log(...args);
-}
-function dlog(...args){
-    if(isDebugLog && isConLog)
-        console.log(...args);
-}
-
-function clogdline(){
-    clog(dline);
-}
-function clogsline(){
-    clog(sline);
-}
-function dlogdline(){
-    dlog(dline);
-}
-function dlogsline(){
-    dlog(sline);
-}
-
-/*
 function to solve the problem
 */
 
@@ -65,7 +30,7 @@ function isPalindrome(n){
         if(sa[i] != sa[j]){
             return false;
         }else{
-            // clog(`near miss: ${n}`)
+            __.dlog(`near miss: ${n}`)
         }
     }
     return true;
@@ -80,28 +45,85 @@ function lpp(){
     let largest = 0;
     let sl = '';
     // for(i=999; i>=100 && !foundit; --i){
+    __.vlog(`Largest Palindromic Products found as we walk through the numbers...`)
     for(i=1; i<=999 && !foundit; ++i){
-            // clog(`i:${i}`);
+        __.dlog(`i:${i}`);
         // for(j=999; j>=100; --j){
         for(j=1; j<=999; ++j){
-            // clog(`i:${i}, j:${j}`)
+            __.dlog(`i:${i}, j:${j}`)
             // process.stdout.write('.');
             test = i*j;
             if(isPalindrome(test)){
                 // foundit = true;
                 if( test>largest){
                     largest = test;
-                    sl = `Palindrome product: ${test} = ${i} * ${j}`;
-                    dlog(sl)
+                    sl = `${test} = ${i} * ${j}`;
+                    __.vlog('Largest so far: '+ sl)
                 }
                 //break;
             }
         }
-        // clog(``);
+        __.dlog(``);
     }
-    clog(sl);
+    __.dlog(sl);
 
-    return 0;
+    return sl;
+}
+
+//
+// faster algorithm.  counting down from 999 * 999
+//
+
+function lpp2(){
+    let test = 0;
+    // by brute force
+    let i=1;
+    let j=1;
+    let lowkey = 999;
+    let foundit = false;
+    let jbreakval = 0;
+    let largest = 0;
+    let previous = 0;
+    let sl = '';
+    __.vlog(`Largest Palindromic Products found as we walk through the numbers...`)
+
+    for(i=999; i>=1 ; --i){
+        __.dlog(`i:${i}`);
+
+        if(jbreakval === 999){ // if the inner loop last gave up at the start, then just give up period
+            __.vlog(`i:${i-1}, j:${j}  BREAK out of outer loop. Previous Test:`,test)
+            __.vlog(`i:${i}, j:${j}  BREAK out of outer loop.  Current Test:`,i*j, `We have found the largest:`, largest)
+            __.vlog(`We will not find a value larger that this:`,i*j, ` as we walk the rest of the values.`)
+
+            break;
+        }
+
+        for(j=999; j>=i; --j){
+            __.dlog(`i:${i}, j:${j}`)
+
+            test = i*j;
+            if(isPalindrome(test)){
+                foundit = true;
+                if( test>largest){
+                    largest = test;
+                    sl = `${test} = ${i} * ${j}`;
+                    if( i+j<lowkey){
+                        lowkey=i+j;
+                    }
+                    __.vlog('Largest so far: '+ sl)
+                }
+            }
+            if(test<largest){
+                __.vlog(`i:${i}, j:${j}  BREAK out of inner loop. test:`, test)
+                jbreakval = j;
+                break;
+            }
+        }
+        __.dlog(``);
+    }
+    __.dlog(sl);
+
+    return sl;
 }
 
 
@@ -109,45 +131,24 @@ function lpp(){
 driver
 */
 
-function parseCommandLineArgs(){
-    let isError = false;
-    let unknownArgs = [];
-    const myArgs = process.argv.slice(2);
-    let debugstrarr=['-d','--debug'];
-    let verbosearr=['-v','--verbose'];
-    for( ; myArgs.length >= 1; myArgs.shift()){
-        // let test=parseInt(myArgs[0]);
-        if( debugstrarr.includes(myArgs[0].toLowerCase()) ){
-            isDebugLog = true;
-            isConLog = true; // what the hell, turn it on here
-            dlog('calling args: ',myArgs)
-            dlog('debug set to TRUE')
-        }
-        else if( verbosearr.includes(myArgs[0].toLowerCase()) ){
-            isDebugLog = true;  // what the hell, turn it on here
-            isConLog = true;
-            clog('calling args: ',myArgs)
-            clog('debug set to TRUE')
-        }
-        else{
-          isError = true;
-          unknownArgs.push(myArgs[0]);
-        }
-        if(isError){
-            console.log();
-            console.log('unknown args:',unknownArgs )
-            // let shelp = myArgs[0].toLowerCase();
-            // if( shelp === '-h' || '--help')
-            console.log();
-            console.log('usage: node lpp.js [-d] [--debug] [-v] [--verbose]');
-            console.log('       -d or --debug. : extra debugging output');
-            console.log('       -v or --verbose: extra chatty output');
-            console.log();
-            return(false);
-        }
-    }
-    return true;
-}
 
-if( ! parseCommandLineArgs() ) return(0);
-lpp();
+if( ! __.parseCommandLineArgs('lpp') ) return(0);
+let largest = lpp();
+
+__.clog()
+__.clog('Find the largest palindrome made from the product of two 3-digit numbers:')
+__.clog()
+__.clog('Naive, brute force... counting up')
+__.clog()
+__.clog('Largest Palindrome Product: ' + largest);
+__.slog(largest);
+
+let largest2 = lpp2();
+__.clog()
+__.clog('Find the largest palindrome made from the product of two 3-digit numbers:')
+__.clog()
+__.clog('less Naive, brute force... counting down')
+__.clog('stopping when we realize the test values are moving away from the Max found so far.')
+__.clog()
+__.clog('Largest Palindrome Product: ' + largest2);
+__.slog(largest2);
